4da334402f8c4736b20b9730365085f9605b62df
TychoLaptop\iMrBa_000 <TychoZaal@gmail.com>
2019-03-04 14:39:33 +0100

    Working in console

---
+++ HanoiTower/HanoiTower/HanoiTower.h
@@ -0,0 +1,11 @@
+#ifndef _HANOI_ //make sure that this header file is only defined once
+#define _HANOI_
+
+class HanoiTower 
+{
+
+public:
+	void Move(int count, int t1, int t3, int t2);
+};
+
+#endif
